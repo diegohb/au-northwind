2a7c875c1341e4dc958c4c4e706509d2705ce277
diegohb@expertvisionsoftware.com <>
2021-04-11 03:29:49 -0400

    - implemented price increase view - created product-price model - created styling - set defaults for model object

--- source/Presentation.WebSPA/src/routes/product-management-spa/product-management-spa.ts
+++ source/Presentation.WebSPA/src/routes/product-management-spa/product-management-spa.ts
@@ -1,9 +1,9 @@
 ﻿import { PLATFORM } from "aurelia-pal";
-import { Router, RouterConfiguration, NavigationInstruction } from "aurelia-router";
+import { Router, RouterConfiguration } from "aurelia-router";
 import { LogManager } from "aurelia-framework"
 
 export class ProductManagementSPAViewModel {
-    private readonly _logger = LogManager.getLogger(this.constructor.name);
+    private readonly _logger: Object = LogManager.getLogger(this.constructor.name);
 
     public router: Router;
 
@@ -14,11 +14,43 @@ export class ProductManagementSPAViewModel {
         //pConfig.options.pushState = true;
         configParam.map([
             { route: "", redirect: "catalog" },
-            { route: "catalog", moduleId: PLATFORM.moduleName("./catalog"), name: "catalog", title: "Catalog", nav: true },
+            {
+                route: "catalog",
+                moduleId: PLATFORM.moduleName("./catalog"),
+                name: "catalog",
+                title: "Catalog",
+                nav: true
+            },
             { route: "sales", moduleId: PLATFORM.moduleName("./sales"), name: "sales", title: "Sales", nav: true },
-            { route: "fulfillment", moduleId: PLATFORM.moduleName("./fulfillment"), name: "fulfillment", title: "Fulfillment", nav: true },
-            { route: "warehouse", moduleId: PLATFORM.moduleName("./warehouse"), name: "warehouse", title: "Warehouse", nav: true },
-            { route: "products/:id", moduleId: PLATFORM.moduleName("./product-summary/index"), name: "product", title: "Product", nav: false }
+            {
+                route: "sales/pricing/:productId/increase",
+                href: "#sales/pricing/:productId/increase",
+                moduleId: PLATFORM.moduleName("./sales/pricing-increase"),
+                name: "sales",
+                title: "Sales",
+                nav: true
+            },
+            {
+                route: "fulfillment",
+                moduleId: PLATFORM.moduleName("./fulfillment"),
+                name: "fulfillment",
+                title: "Fulfillment",
+                nav: true
+            },
+            {
+                route: "warehouse",
+                moduleId: PLATFORM.moduleName("./warehouse"),
+                name: "warehouse",
+                title: "Warehouse",
+                nav: true
+            },
+            {
+                route: "products/:id",
+                moduleId: PLATFORM.moduleName("./product-summary/index"),
+                name: "product",
+                title: "Product",
+                nav: false
+            }
         ]);
 
     }
---
+++ source/Presentation.WebSPA/src/routes/product-management-spa/sales/pricing-increase.ts
@@ -0,0 +1,25 @@
+﻿import { autoinject, LogManager, bindable } from "aurelia-framework";
+import { ProductPriceModel } from "./models/product-price-model";
+
+@autoinject
+export class PricingIncreaseViewModel {
+    private readonly _logger: Object = LogManager.getLogger(this.constructor.name);
+
+    public model: ProductPriceModel = new ProductPriceModel();
+
+    @bindable
+    public newPrice: number;
+
+    public comment: string = "";
+
+    public async commit(): Promise<void> {
+        this.model.increasePrice(this.newPrice, this.comment);
+        this.newPrice = null;
+        this.comment = "";
+    }
+
+    public async cancel(): Promise<void> {
+        this.newPrice = null;
+        this.comment = "";
+    }
+}
