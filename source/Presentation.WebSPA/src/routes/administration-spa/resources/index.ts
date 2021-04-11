75dd665f300aa39bf7bf5cf9ed598f85c2f2dcff
diegohb@expertvisionsoftware.com <>
2021-04-11 15:26:39 -0400

    rename spa long name to shorter administration-spa

--- source/Presentation.WebSPA/src/routes/administration-spa/resources/index.ts
+++ source/Presentation.WebSPA/src/routes/administration-spa/resources/index.ts
@@ -3,6 +3,6 @@
 export function configure(config: FrameworkConfiguration) {
     //config.globalResources([]);
 
-    const spaFeaturesPath: string = "routes/product-management-spa/resources/features";
+    const spaFeaturesPath: string = "routes/administration-spa/resources/features";
     config.feature(PLATFORM.moduleName(`${spaFeaturesPath}/test-feature`));
 }
