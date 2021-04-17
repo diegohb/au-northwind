a7f49ed663d212d130b7708cdbc0c529368ba565
diegohb@expertvisionsoftware.com <>
2021-04-17 17:34:01 -0400

    refactored for product listing in catalog

--- source/Presentation.WebSPA/src/apps/administration-spa/catalog/catalog.ts
+++ source/Presentation.WebSPA/src/apps/administration-spa/catalog/catalog.ts
@@ -1,4 +1,11 @@
-﻿export class CatalogVM
-{
+﻿import { bindable } from "aurelia-framework";
+import { ProductListItem } from "../models/product-list-item";
 
+export class CatalogVM {
+    @bindable
+    public products: Array<ProductListItem> = [
+        { sku: "ABC123", name: "A Valuable Book", lastUpdate: "2 days ago" },
+        { sku: "DEF456", name: "Stethoscope", lastUpdate: "10 days ago" },
+        { sku: "GHI789", name: "Box of Chocolates", lastUpdate: "1 day ago" }
+    ];
 }
--- source/Presentation.WebSPA/src/apps/administration-spa/sales/sales.ts
+++ source/Presentation.WebSPA/src/apps/administration-spa/sales/sales.ts
@@ -1,8 +1,3 @@
-﻿import { bindable } from "aurelia-framework";
-import { ProductListItem } from "../models/product-list-item";
+﻿export class PricingViewModel {
 
-export class PricingViewModel {
-
-    @bindable
-    public products: Array<ProductListItem> = [{ sku: "ABC123", name: "A Valuable Book", lastUpdate: "2 days ago" }];
 }
