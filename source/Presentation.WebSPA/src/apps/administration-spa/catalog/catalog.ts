2418a8d62fe2d4cae32c506ff0ce0200eea29486
diegohb@expertvisionsoftware.com <>
2021-04-18 20:05:34 -0400

    still messing with navigation

--- source/Presentation.WebSPA/src/apps/administration-spa/catalog/catalog.ts
+++ source/Presentation.WebSPA/src/apps/administration-spa/catalog/catalog.ts
@@ -4,8 +4,8 @@ import { ProductListItem } from "../models/product-list-item";
 export class CatalogVM {
     @bindable
     public products: Array<ProductListItem> = [
+        { sku: "GHI789", name: "Box of Chocolates", lastUpdate: "1 day ago" },
         { sku: "ABC123", name: "A Valuable Book", lastUpdate: "2 days ago" },
-        { sku: "DEF456", name: "Stethoscope", lastUpdate: "10 days ago" },
-        { sku: "GHI789", name: "Box of Chocolates", lastUpdate: "1 day ago" }
+        { sku: "DEF456", name: "Stethoscope", lastUpdate: "10 days ago" }
     ];
 }
