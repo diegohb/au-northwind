1219dba7b7287ada6cb0eff822a9b4f40ead737e
diegohb@expertvisionsoftware.com <>
2021-04-18 21:41:52 -0400

    dynamic click behavior for product-listing custom element

--- source/Presentation.WebSPA/src/apps/administration-spa/catalog/catalog.ts
+++ source/Presentation.WebSPA/src/apps/administration-spa/catalog/catalog.ts
@@ -1,11 +1,23 @@
-﻿import { bindable } from "aurelia-framework";
+﻿import { autoinject, bindable } from "aurelia-framework";
+import { Router } from "aurelia-router";
 import { ProductListItem } from "../models/product-list-item";
 
+@autoinject()
 export class CatalogVM {
+    private _router: Router;
+
+    constructor(routerParam: Router) {
+        this._router = routerParam;
+    }
+
     @bindable
     public products: Array<ProductListItem> = [
         { sku: "GHI789", name: "Box of Chocolates", lastUpdate: "1 day ago" },
         { sku: "ABC123", name: "A Valuable Book", lastUpdate: "2 days ago" },
         { sku: "DEF456", name: "Stethoscope", lastUpdate: "10 days ago" }
     ];
+
+    public async navigateToProduct(itemParam: ProductListItem): Promise<boolean> {
+        return this._router.navigateToRoute("product", { id: itemParam.sku });
+    }
 }
--- source/Presentation.WebSPA/src/apps/administration-spa/resources/custom-elements/product-listing.ts
+++ source/Presentation.WebSPA/src/apps/administration-spa/resources/custom-elements/product-listing.ts
@@ -7,5 +7,13 @@ export class ProductListingCustomElement {
     private readonly _logger: Logger = LogManager.getLogger(this.constructor.name);
 
     @bindable()
+    public onClick: Function;
+
+    @bindable()
     public products: Array<ProductListItem> = new Array<ProductListItem>();
+
+    public async performClick(itemParam: ProductListItem): Promise<void> {
+        return await this.onClick({ itemParam: itemParam });
+    }
+
 }
