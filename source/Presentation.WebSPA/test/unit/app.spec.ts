33c36f6193e0276c8e61dbc22f3bd4f2457fad7b
diegohb@expertvisionsoftware.com <>
2021-04-25 17:19:45 -0400

    refactor test

--- source/Presentation.WebSPA/test/unit/app.spec.ts
+++ source/Presentation.WebSPA/test/unit/app.spec.ts
@@ -1,21 +1,30 @@
 import { AdminSPAViewModel } from "../../src/apps/administration-spa/app";
 import { ShoppingSPAViewModel } from "../../src/apps/shopping-spa/app";
+import { RouterConfiguration } from "aurelia-router";
 
 describe("admin spa",
     () => {
+        let spa: AdminSPAViewModel;
+        beforeEach(() => {
+            spa = new AdminSPAViewModel();
+        });
         it("router configures",
-            () => {
-                const spa: AdminSPAViewModel = new AdminSPAViewModel();
+            async () => {
+                await spa.configureRouter(new RouterConfiguration(), null);
                 expect(spa.router).toBeDefined();
             });
     });
 
 describe("shopping spa",
     () => {
+        let spa: ShoppingSPAViewModel;
+        beforeEach(() => {
+            spa = new ShoppingSPAViewModel();
+        });
+
         it("router configures",
             async () => {
-                const spa: ShoppingSPAViewModel = new ShoppingSPAViewModel();
-                //await spa.configureRouter(null, new Router());
+                await spa.configureRouter(new RouterConfiguration(), null);
                 expect(spa.router).toBeDefined();
             });
     });
