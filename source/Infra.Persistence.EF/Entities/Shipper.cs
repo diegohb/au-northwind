a6e56cf796c7037ea63ec23a2adc6d85131a5e18
diegohb@expertvisionsoftware.com <>
2021-12-05 23:08:34 -0500

    add ef mapping and context for northwind

---
+++ source/Infra.Persistence.EF/Entities/Shipper.cs
@@ -0,0 +1,13 @@
+﻿namespace Infra.Persistence.EF.Entities
+{
+  using System.Collections.Generic;
+
+  public class Shipper
+  {
+    // related entities
+    public ICollection<Order> Orders { get; set; }
+    public string Phone { get; set; }
+    public int ShipperID { get; set; }
+    public string ShipperName { get; set; }
+  }
+}
