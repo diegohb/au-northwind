8c90e39fa3d1b56fc6292dc4c6501a1e53d3b090
diegohb@expertvisionsoftware.com <>
2022-01-15 17:26:20 -0500

    reorg props and regions

--- source/Infra.Persistence.EF/NorthwindDbContext.cs
+++ source/Infra.Persistence.EF/NorthwindDbContext.cs
@@ -418,33 +418,38 @@ public class NorthwindDbContext : DbContext
     });
   }
 
-  #region Properties
+  #region Query View Properties
 
   public virtual DbSet<AlphabeticalListOfProductView> AlphabeticalListOfProducts { get; set; }
-  public virtual DbSet<Category> Categories { get; set; }
   public virtual DbSet<CategorySalesFor1997View> CategorySalesFor1997s { get; set; }
   public virtual DbSet<CurrentProductListView> CurrentProductLists { get; set; }
   public virtual DbSet<CustomerAndSuppliersByCityView> CustomerAndSuppliersByCities { get; set; }
-  public virtual DbSet<CustomerDemographic> CustomerDemographics { get; set; }
-  public virtual DbSet<Customer> Customers { get; set; }
-  public virtual DbSet<Employee> Employees { get; set; }
-  public virtual DbSet<InvoiceView> Invoices { get; set; }
-  public virtual DbSet<OrderDetail> OrderDetails { get; set; }
   public virtual DbSet<OrderDetailsExtendedView> OrderDetailsExtendeds { get; set; }
-  public virtual DbSet<Order> Orders { get; set; }
   public virtual DbSet<OrdersQryView> OrdersQries { get; set; }
   public virtual DbSet<OrderSubtotalView> OrderSubtotals { get; set; }
-  public virtual DbSet<Product> Products { get; set; }
   public virtual DbSet<ProductsAboveAveragePriceView> ProductsAboveAveragePrices { get; set; }
   public virtual DbSet<ProductSalesFor1997View> ProductSalesFor1997s { get; set; }
   public virtual DbSet<ProductsByCategoryView> ProductsByCategories { get; set; }
   public virtual DbSet<QuarterlyOrderView> QuarterlyOrders { get; set; }
-  public virtual DbSet<Region> Regions { get; set; }
   public virtual DbSet<SalesByCategoryView> SalesByCategories { get; set; }
   public virtual DbSet<SalesTotalsByAmountView> SalesTotalsByAmounts { get; set; }
-  public virtual DbSet<Shipper> Shippers { get; set; }
   public virtual DbSet<SummaryOfSalesByQuarterView> SummaryOfSalesByQuarters { get; set; }
   public virtual DbSet<SummaryOfSalesByYearView> SummaryOfSalesByYears { get; set; }
+
+  #endregion
+
+  #region Entity Properties
+
+  public virtual DbSet<InvoiceView> Invoices { get; set; }
+  public virtual DbSet<Category> Categories { get; set; }
+  public virtual DbSet<CustomerDemographic> CustomerDemographics { get; set; }
+  public virtual DbSet<Customer> Customers { get; set; }
+  public virtual DbSet<Employee> Employees { get; set; }
+  public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+  public virtual DbSet<Order> Orders { get; set; }
+  public virtual DbSet<Product> Products { get; set; }
+  public virtual DbSet<Region> Regions { get; set; }
+  public virtual DbSet<Shipper> Shippers { get; set; }
   public virtual DbSet<Supplier> Suppliers { get; set; }
   public virtual DbSet<Territory> Territories { get; set; }
 
