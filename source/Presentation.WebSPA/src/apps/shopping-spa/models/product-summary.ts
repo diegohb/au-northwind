eea8605c3e510ef92e3827e66f510668a22ee48c
diegohb@expertvisionsoftware.com <>
2022-02-19 21:08:56 -0500

    - stubbed out product svc - dto object for product api obj - model for product summary

---
+++ source/Presentation.WebSPA/src/apps/shopping-spa/models/product-summary.ts
@@ -0,0 +1,22 @@
+﻿import { ProductDTO } from "./product-dto";
+
+export class ProductSummaryModel {
+    public constructor(
+        private readonly _sku: string,
+        private readonly _name: string,
+        private readonly _price: number,
+        private readonly _description?: string) {
+    }
+
+    public get sku(): string { return this._sku; }
+
+    public get name(): string { return this._name; }
+
+    public get price(): number { return this._price; }
+
+    public get description(): string { return this._description; }
+
+    public static fromDTO(dtoParam: ProductDTO) {
+        return new ProductSummaryModel(dtoParam.sku, dtoParam.productName, dtoParam.unitPrice, dtoParam.description);
+    }
+}
---
+++ source/Presentation.WebSPA/src/apps/shopping-spa/services/product-service.ts
@@ -0,0 +1,15 @@
+﻿import { ProductSummaryModel } from "../models/product-summary";
+import { ProductDTO } from "../models/product-dto";
+
+export class ProductService {
+
+    public async getShoppableProductSummaries(): Promise<Array<ProductSummaryModel>> {
+        const products = await this.fetchProducts();
+        const models = products.map(dto => ProductSummaryModel.fromDTO(dto));
+        return models;
+    }
+
+    private async fetchProducts(): Promise<ProductDTO[]> {
+        return [];
+    }
+}
