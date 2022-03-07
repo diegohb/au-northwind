b8506a494b7726b1ce696505cd50dd5eeb5115b9
diegohb@expertvisionsoftware.com <>
2022-03-07 17:54:33 -0500

    comments

--- source/Northwind.Core/Domain/IDomainMediator.cs
+++ source/Northwind.Core/Domain/IDomainMediator.cs
@@ -1,10 +1,15 @@
 ﻿namespace Northwind.Core.Domain;
 
-public interface IDomainMediator<TAggregateId>
-  where TAggregateId : IIdentityValueObject
+/// <summary>
+///   This is a mediator implementation that will publish domain events internally for consumption by other aggregates,
+///   services, and higher logical layer handlers.
+/// </summary>
+/// <typeparam name="TIdentity">The ID of the aggregate or entity associated to the domain event.</typeparam>
+public interface IDomainMediator<TIdentity>
+  where TIdentity : IIdentityValueObject
 {
-  Task PublishAsync(IDomainEvent<TAggregateId> notificationParam, CancellationToken cancellationTokenParam = default);
+  Task PublishAsync(IDomainEvent<TIdentity> notificationParam, CancellationToken cancellationTokenParam = default);
 
   Task PublishAsync<TNotification>(TNotification notificationParam, CancellationToken cancellationTokenParam = default)
-    where TNotification : IDomainEvent<TAggregateId>;
+    where TNotification : IDomainEvent<TIdentity>;
 }
