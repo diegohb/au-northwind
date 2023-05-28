ab32494fe1729555c94275a9ad0c7ab1cd54fe57
diegohb@expertvisionsoftware.com <>
2023-05-28 19:25:38 -0400

    - use mediatr for products controller - crerate spec for productBySku

---
+++ source/Northwind.Application/Products/GetAllProductsQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace Northwind.Application.Products;
+
+using Abstractions;
+using Infra.Persistence.EF.Entities;
+
+public record GetAllProductsQuery : IQuery<IList<Product>>;
+
+public record GetProductBySku(Guid ProductId) : IQuery<Product>;
