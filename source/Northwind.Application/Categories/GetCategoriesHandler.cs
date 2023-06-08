e8dd57c8f0fdb56e1f5a3f18c71ad4b12c482062
diegohb@expertvisionsoftware.com <>
2023-06-08 01:55:10 -0400

    - installed erroror - updated query and query handler abstractions - implemented changes, compiles

--- source/Northwind.Application/Categories/GetCategoriesHandler.cs
+++ source/Northwind.Application/Categories/GetCategoriesHandler.cs
@@ -2,12 +2,13 @@
 
 using Abstractions;
 using Core.Persistence;
+using ErrorOr;
 using Infra.Persistence.EF.Entities;
 
 public class GetCategoriesHandler
-  : IQueryHandler<GetCategoriesQuery, IList<CatalogCategoryDTO>>,
-    IQueryHandler<GetCategoryByIDQuery, CatalogCategoryDTO?>,
-    IQueryHandler<GetCategoryByNameQuery, CatalogCategoryDTO?>
+  : IQueryHandler<GetCategoriesQuery, ErrorOr<IList<CatalogCategoryDTO>>>,
+    IQueryHandler<GetCategoryByIDQuery, ErrorOr<CatalogCategoryDTO>>,
+    IQueryHandler<GetCategoryByNameQuery, ErrorOr<CatalogCategoryDTO>>
 {
   private readonly IQueryRepository<Category, int> _queryRepo;
 
@@ -16,29 +17,40 @@ public class GetCategoriesHandler
     _queryRepo = queryRepositoryParam;
   }
 
-  public async Task<IList<CatalogCategoryDTO>> Handle(GetCategoriesQuery queryParam, CancellationToken cancellationTokenParam)
+  public async Task<ErrorOr<IList<CatalogCategoryDTO>>> Handle(GetCategoriesQuery queryParam, CancellationToken cancellationTokenParam)
   {
     var activeCategories = await _queryRepo.GetAllAsync();
-    return activeCategories.Select(getDTOFromEntity).ToList();
+    var categories = activeCategories.ToList();
+    if (!categories.Any())
+    {
+      return Error.NotFound();
+    }
+
+    return categories.Select(getDTOFromEntity).ToList();
   }
 
-  public async Task<CatalogCategoryDTO?> Handle(GetCategoryByIDQuery queryParam, CancellationToken cancellationTokenParam)
+  public async Task<ErrorOr<CatalogCategoryDTO>> Handle(GetCategoryByIDQuery queryParam, CancellationToken cancellationTokenParam)
   {
     var categoryEntity = await _queryRepo.GetByIdAsync(queryParam.CategoryID);
+    if (categoryEntity == null)
+    {
+      return Error.NotFound();
+    }
+
     return getDTOFromEntity(categoryEntity);
   }
 
-  public async Task<CatalogCategoryDTO?> Handle(GetCategoryByNameQuery queryParam, CancellationToken cancellationTokenParam)
+  public async Task<ErrorOr<CatalogCategoryDTO>> Handle(GetCategoryByNameQuery queryParam, CancellationToken cancellationTokenParam)
   {
     var categories = (await _queryRepo.FindBySpecificationAsync(new CategoryByNameSpec(queryParam.Name))).ToList();
     if (categories.Count > 1)
     {
-      throw new MoreThan1CategoryByNameException(queryParam.Name, categories.Count);
+      return Error.Unexpected(description: $"More than 1 category found by the name '{queryParam.Name}'.");
     }
 
     var categoryEntity = categories.SingleOrDefault();
     return categoryEntity == null
-      ? null
+      ? Error.NotFound()
       : getDTOFromEntity(categoryEntity);
   }
 
